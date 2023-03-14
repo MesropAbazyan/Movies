0cccf6bbd5bfa770df98a062c327fb93a7ece030
Mesrop Abazyan <107545114+MesropAbazyan@users.noreply.github.com>
2023-03-14 16:12:01 +0400

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import Navbar from '../components/Navbar';
 import Filters from '../components/Filters';
 import Posts from '../components/Posts';
 import Footer from '../components/Footer';
+import SearchResult from '../components/SearchResult';
 
 function Home() {
   return (
     <>
       <Navbar />
       <Filters />
+      <SearchResult />
       <Posts />
       <Footer />
     </>
